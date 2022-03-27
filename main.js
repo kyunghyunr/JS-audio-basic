3e7555317a06833e6db94760e2d3c573db36ef5b
atilla <atilla@atillas-MacBook-Pro.local>
2022-03-27 18:34:56 +0900

    #02

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 
 const container = document.getElementById('container');
 const canvas = document.getElementById('canvas1');
+const file = document.getElementById('fileupload');
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
@@ -20,11 +22,11 @@ container.addEventListener('click', function(){
 	analyser = audioContext.createAnalyser();
 	audioSource.connect(analyser);
 	analyser.connect(audioContext.destination);
-	analyser.fftSize = 64;
+	analyser.fftSize = 512;
 	const bufferLength = analyser.frequencyBinCount;
 	const dataArray = new Uint8Array(bufferLength);
 
-	const barWidth = canvas.width/bufferLength;
+	const barWidth = (canvas.width/2)/bufferLength;
 	let barHeight;
 	let x;
 
@@ -32,13 +34,53 @@ container.addEventListener('click', function(){
 		x = 0;
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		analyser.getByteFrequencyData(dataArray);
-		for (let i = 0; i < bufferLength; i++){
-			barHeight = dataArray[i];
-			ctx.fillStyle = 'white';
-			ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-			x += barWidth;
-		}
+		drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray)
 		requestAnimationFrame(animate);
 	}
 	animate();
 });
+
+file.addEventListener('change', function(){
+	const files = this.files;
+	const audio1 = document.getElementById('audio1');
+	audio1.src = URL.createObjectURL(files[0]);
+	audio1.load();
+	audio1.play();
+
+	audioSource = audioContext.createMediaElementSource(audio1);
+	analyser = audioContext.createAnalyser();
+	audioSource.connect(analyser);
+	analyser.connect(audioContext.destination);
+	analyser.fftSize = 512;
+	const bufferLength = analyser.frequencyBinCount;
+	const dataArray = new Uint8Array(bufferLength);
+
+	const barWidth = (canvas.width/2)/bufferLength;
+	let barHeight;
+	let x;
+
+	function animate(){
+		x = 0;
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		analyser.getByteFrequencyData(dataArray);
+		drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray)
+		requestAnimationFrame(animate);
+	}
+	animate();
+
+
+});
+
+function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray){
+	for (let i = 0; i < bufferLength; i++){
+		barHeight = dataArray[i] * 4;
+		ctx.save();
+		ctx.translate(canvas.width/2, canvas.height/2);
+		ctx.rotate(i + Math.PI * 8 / bufferLength);
+		const hue = i * 500;
+		ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';
+		ctx.fillRect(0, 0, barWidth, barHeight);
+		x += barWidth;
+		ctx.restore();
+	};
+};
